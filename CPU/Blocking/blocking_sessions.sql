0cc8649ca0cdb878c35c6a571c7fd328d78908f0
rohanmike9181-crypto <rohanmike9181@gmail.com>
2026-05-13 14:13:23 +0530

    Delete CPU/Blocking directory

--- CPU/Blocking/blocking_sessions.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    blocking_session_id AS BlockingSessionID,
-    session_id AS BlockedSessionID,
-    wait_type,
-    wait_time,
-    wait_resource
-FROM sys.dm_exec_requests
-WHERE blocking_session_id <> 0;
